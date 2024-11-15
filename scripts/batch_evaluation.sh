053c2972074842f049f869f15d62afc0249719f4
hae-sung-oh <ohs980111@gmail.com>
2024-11-15 10:56:20 +0900

    Added date label in evaluation

--- scripts/batch_evaluation.sh
+++ scripts/batch_evaluation.sh
@@ -2,10 +2,12 @@
 
 source set_environment.sh
 
+DATE = $(date '+%y%m%d-%H%M')
+
 for i in {1..36}; do
   echo "Starting index $i"
-  export CHECKPOINT="${WORK_DIR}/result/simulation_results_${i}.json"
-  export RESULT_LIST="${WORK_DIR}/result/result_list_${i}.pickle"
+  export CHECKPOINT="${WORK_DIR}/result/${DATE}/simulation_results_${i}_${DATE}.json"
+  export RESULT_LIST="${WORK_DIR}/result/pkl/result_list_${i}.pickle"
   export VEHICLEINDEX=$i
   ./evaluate.sh
 done

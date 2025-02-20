bc0507d87337316c5f5a73a38601d0e9554c966b
hae-sung-oh <ohs980111@gmail.com>
2025-02-20 11:44:35 +0900

    Added team_code_mvadapt directory

--- scripts/set_environment.sh
+++ scripts/set_environment.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-export CARLA_ROOT=/path/to/carla
-export WORK_DIR=/path/to/mvadapt
+export CARLA_ROOT=/home/ohs-dyros/carla
+export WORK_DIR=/home/ohs-dyros/gitRepo/MVAdapt
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
 export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.12-py3.8-linux-x86_64.egg
 export TEAM_CODE_ROOT=${WORK_DIR}/team_code
+export TEAM_CODE_MVADAPT=${WORK_DIR}/team_code_mvadapt
 export SCENARIO_RUNNER_ROOT=${WORK_DIR}/scenario_runner
 export LEADERBOARD_ROOT=${WORK_DIR}/leaderboard
-export PYTHONPATH=$PYTHONPATH:$SCENARIO_RUNNER_ROOT:$LEADERBOARD_ROOT:$TEAM_CODE_ROOT:$WORK_DIR
+export PYTHONPATH=$PYTHONPATH:$SCENARIO_RUNNER_ROOT:$LEADERBOARD_ROOT:$TEAM_CODE_ROOT:$TEAM_CODE_MVADAPT:$WORK_DIR

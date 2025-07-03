2655c5100d45cb9fba0317ae4cd25fdb1c43550e
hae-sung-oh <ohs980111@gmail.com>
2025-07-03 14:12:25 +0900

    Shell script updates

--- scripts/finetune_mvadapt.sh
+++ scripts/finetune_mvadapt.sh
@@ -7,7 +7,7 @@ export AGENTCONFIG="${WORK_DIR}/pretrained_models/leaderboard/tfpp_wp_all_0"
 export UNCERTAINTY_THRESHOLD=0.33
 export STOP_CONTROL=0
 
-export VEHICLE=6
+export VEHICLE=6 # carlamotors.carlacola
 export ROOT_DIR="${WORK_DIR}/dataset"
 export BASE_MODEL=$AGENTCONFIG
 export DEVICE="cuda:0"
@@ -17,10 +17,10 @@ export BATCH_SIZE=512
 export PROCESS_BATCH=64
 export VERBOSE=true
 
-export LOAD_DATA="${WORK_DIR}/dataset/mvadapt_finetune_dataset"
-export SAVE_DATA="None"
-# export LOAD_DATA="None"
-# export SAVE_DATA="${WORK_DIR}/dataset/mvadapt_finetune_dataset"
+# export LOAD_DATA="${WORK_DIR}/dataset/mvadapt_finetune_dataset"
+# export SAVE_DATA="None"
+export LOAD_DATA="None"
+export SAVE_DATA="${WORK_DIR}/dataset/mvadapt_finetune_dataset"
 
 export PRETRAINED_MODEL="${WORK_DIR}/pretrained_models/mvadapt.pth"
 export SAVE_FINETUNED_MODEL="${WORK_DIR}/pretrained_models/mvadapt_finetuned.pth"
